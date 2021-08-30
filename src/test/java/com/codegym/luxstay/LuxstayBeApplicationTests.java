17621801ddc58d8f314a0e65ea4592861869dc2d
thantai01 <thantaihp2011@gmail.com>
2021-08-31 01:13:28 +0700

    cau hinh xong gradle & mysql

---
+++ src/test/java/com/codegym/luxstay/LuxstayBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codegym.luxstay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LuxstayBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
