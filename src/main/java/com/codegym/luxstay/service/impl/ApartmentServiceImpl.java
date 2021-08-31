5fc3824d0f7f931ebfaaa8e26032db2dfb236fc7
kienk93 <kienk93@gmail.com>
2021-08-31 16:35:01 +0700

    add CRUD apartment

--- src/main/java/com/codegym/luxstay/service/impl/ApartmentServiceImpl.java
+++ src/main/java/com/codegym/luxstay/service/impl/ApartmentServiceImpl.java
@@ -1,2 +1,59 @@
-package com.codegym.luxstay.service.impl;public class ApartmentServiceImpl {
+package com.codegym.luxstay.service.impl;
+
+import com.codegym.luxstay.model.Apartment;
+import com.codegym.luxstay.repository.IApartmentRepository;
+import com.codegym.luxstay.service.iservice.IApartment;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class ApartmentServiceImpl implements IApartment {
+    @Autowired
+    IApartmentRepository iApartmentRepository;
+    @Override
+    public Iterable<Apartment> findAll() {
+        return iApartmentRepository.findAll();
+    }
+
+    @Override
+    public Optional<Apartment> findById(long id) {
+        return iApartmentRepository.findById(id);
+    }
+
+    @Override
+    public Apartment save(Apartment apartment) {
+        return iApartmentRepository.save(apartment);
+    }
+
+    @Override
+    public void delete(long id) {
+        iApartmentRepository.deleteById(id);
+    }
+
+    @Override
+    public Iterable<Apartment> findAllByNameContaining(String name) {
+        return iApartmentRepository.findAllByNameContaining(name);
+    }
+
+    @Override
+    public Iterable<Apartment> findAllByStatusContaining(String status) {
+        return iApartmentRepository.findAllByStatusContaining(status);
+    }
+
+    @Override
+    public Iterable<Apartment> findAllByPriceContaining(String price) {
+        return iApartmentRepository.findAllByPriceContaining(price);
+    }
+
+    @Override
+    public Iterable<Apartment> findAllByApartmentTypeContaining(String type) {
+        return iApartmentRepository.findAllByApartmentTypeContaining(type);
+    }
+
+    @Override
+    public Iterable<Apartment> findAllByDescriptionContaining(String description) {
+        return iApartmentRepository.findAllByDescriptionContaining(description);
+    }
 }
--- src/main/java/com/codegym/luxstay/service/iservice/IApartment.java
+++ src/main/java/com/codegym/luxstay/service/iservice/IApartment.java
@@ -1,2 +1,12 @@
-package com.codegym.luxstay.service.iservice;public interface IApartment {
+package com.codegym.luxstay.service.iservice;
+
+import com.codegym.luxstay.model.Apartment;
+import com.codegym.luxstay.service.IGeneralService;
+
+public interface IApartment extends IGeneralService<Apartment> {
+    Iterable<Apartment> findAllByNameContaining(String name);
+    Iterable<Apartment> findAllByStatusContaining(String status);
+    Iterable<Apartment> findAllByPriceContaining(String price);
+    Iterable<Apartment> findAllByApartmentTypeContaining(String type);
+    Iterable<Apartment> findAllByDescriptionContaining(String description);
 }
