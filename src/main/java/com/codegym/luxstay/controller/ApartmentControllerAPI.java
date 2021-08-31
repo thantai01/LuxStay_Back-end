662fec1121b4ae8081e8c422397af2b82fcba347
kienk93 <kienk93@gmail.com>
2021-08-31 16:16:56 +0700

    pull

--- src/main/java/com/codegym/luxstay/controller/ApartmentControllerAPI.java
+++ src/main/java/com/codegym/luxstay/controller/ApartmentControllerAPI.java
@@ -1,2 +1,54 @@
-package com.codegym.luxstay.controller;public class ApartmentControllerAPI {
+package com.codegym.luxstay.controller;
+
+import com.codegym.luxstay.model.Apartment;
+import com.codegym.luxstay.service.impl.ApartmentServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/apartments")
+public class ApartmentControllerAPI {
+    @Autowired
+    ApartmentServiceImpl apartmentService;
+
+    @GetMapping("")
+    public ResponseEntity<Iterable<Apartment>> getAllApartment() {
+        return new ResponseEntity<>(apartmentService.findAll(), HttpStatus.OK);
+    }
+
+    @PostMapping("")
+    public ResponseEntity<Apartment> saveApartment(@RequestBody Apartment apartment) {
+        return new ResponseEntity<>(apartmentService.save(apartment), HttpStatus.CREATED);
+    }
+    @GetMapping("/${id}")
+    public ResponseEntity<Apartment> findById(@PathVariable Long id) {
+        Optional<Apartment> apartmentOptional = apartmentService.findById(id);
+        if(!apartmentOptional.isPresent()) {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+        return new ResponseEntity<>(apartmentOptional.get(),HttpStatus.OK);
+    }
+    @PutMapping("/${id}")
+    public ResponseEntity<Apartment> editById(@PathVariable Long id,@RequestBody Apartment apartment) {
+        Optional<Apartment> apartmentOptional = apartmentService.findById(id);
+        if(!apartmentOptional.isPresent()) {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+        apartment.setId(apartmentOptional.get().getId());
+        apartmentService.save(apartment);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+    @DeleteMapping("/${id}")
+    public ResponseEntity<Apartment> deleteById(@PathVariable Long id) {
+        Optional<Apartment>  apartmentOptional = apartmentService.findById(id);
+        if(!apartmentOptional.isPresent()) {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+        apartmentService.delete(id);
+        return  new ResponseEntity<>(HttpStatus.OK);
+    }
 }
