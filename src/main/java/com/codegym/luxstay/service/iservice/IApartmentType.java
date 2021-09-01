a0574913cc1a667faf8efad402832692c95661b6
kienk93 <kienk93@gmail.com>
2021-09-01 09:37:46 +0700

    API apartmenttype

---
+++ src/main/java/com/codegym/luxstay/service/iservice/IApartmentType.java
@@ -0,0 +1,7 @@
+package com.codegym.luxstay.service.iservice;
+
+import com.codegym.luxstay.model.ApartmentType;
+import com.codegym.luxstay.service.IGeneralService;
+
+public interface IApartmentType extends IGeneralService<ApartmentType> {
+}
